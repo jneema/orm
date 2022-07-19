4987a7ae260e95e290f403ae539c6a4b013ca849
jneema <joeyshem33@gmail.com>
2022-07-19 13:00:43 +0300

    pushing

--- configs/base_config.py
+++ configs/base_config.py
@@ -1,21 +1,15 @@
-from distutils.debug import DEBUG
-from re import T
-import secrets
 import os
 
-class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'some_secret'
-    environment = 'Development'
-    DEBUG = True
+class Development():
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = os.getenv("DEBUG")
+    FLASK_APP = os.getenv("FLASK_APP")
+    FLASK_ENV = os.getenv("FLASK_ENV")
+    
 
-class Development(Config):
-    environment = 'Development'
-    DEBUG = True
+class Production():
+    SQLALCHEMY_DATABASE_URI = "postgresql://wqsvbcbvszvkxi:de81e26baa5b0b4a8d7ec3ec0cbc0e9617d8a8e9a20cc9c62de166118ab6945a@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dccs4uikrqga4a"
 
-class Testing(Config):
-    DEBUG = False
-
-class Production(Config):
-    Debug = False
-    environment = 'Production'
+    SECRET_KEY = 'LongAndRandomSecretKey'
